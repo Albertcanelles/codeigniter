155ad8d0a3d1a8b52d6b243a2308eff2552a8ff8
root <root@albert-pc.(none)>
2013-10-04 23:54:32 +0200

    Estils Aplicats menu i footer posat remodelat la llista falta canviar nom de les classes

--- application/views/listusers.php
+++ application/views/listusers.php
@@ -1,29 +1,39 @@
 <html>
 <head>
 
-<!-- DataTables CSS -->
-<link rel="stylesheet" type="text/css" href="http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.4/css/jquery.dataTables.css">
- 
-<!-- jQuery -->
-<script type="text/javascript" charset="utf8" src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.8.2.min.js"></script>
- 
-<!-- DataTables -->
-<script type="text/javascript" charset="utf8" src="http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.4/jquery.dataTables.min.js"></script>
+<?php include("bootstrap.txt"); ?>
 
-<script type="text/javascript">
- $(document).ready(function() {
-    $('#taula').dataTable();
- } );
-</script>
 
-<!-- DataTables CSS -->
-<link rel="stylesheet" type="text/css" href="http://www.datatables.net/release-datatables/media/css/demo_table.css">
-<link rel="stylesheet" type="text/css" href="http://www.datatables.net/release-datatables/media/css/demo_page.css">
-
-<t1> Encara no ets membre fes click aqui <a href="createusers"><input type="button" name="boton" value="Registrat"/> </t1></a></br>
 </head>
 <body>
-<table id="taula" class="display" border="1">
+	<div class="navbar navbar-inverse navbar-fixed-top">
+	<div class="container">
+        <div class="navbar-header">
+          <button data-target=".navbar-collapse" data-toggle="collapse" class="navbar-toggle" type="button">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a href="#" class="navbar-brand">Llista Usuaris</a>
+        </div>
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="createusers">Registrat</a></li>
+            <li><a href="createusers">Canvia contrasenya</a></li>
+            <li><a href="modifyusers">Modifica l'usuari</a></li>
+            <li><a href="/codeigniterhelloworld/application/views/pagina.php">Menu Principal</a></li>
+          </ul>
+        </div>
+      </div>
+      </div>
+<div>
+</div>
+</br>
+</br>
+</br>
+</br>
+Encara no ets membre fes click aqui <a href="createusers"><input type="button" class="btn btn-sm btn-success"  name="boton" value="Registrat"/></a></br>
+<table id="taula" class="table table-hover table-condensed" border="1">
 <thead>
         <tr>
 	    <th>id</th>
@@ -42,8 +52,8 @@
             <td>albertcanelles@iesebre.com</td>
             <td>19/03/1993</td>
             <td>No</td>
-   	    <td><a href="modifyusers/1"><input type="button" name="boton" value="Modificar"/></a>
-	    <a href="deleteusers/1"><input type="button" name="boton" value="Eliminar"/></td></a>	
+   	    <td><a href="modifyusers/1"><input type="button" name="boton" class="btn btn-sm btn-primary" value="Modificar"/></a>
+	    <a href="deleteusers/1"><input type="button" class="btn btn-sm btn-danger" name="boton" value="Eliminar"/></td></a>	
         </tr>
         <tr>
 	    <td>2</td>
@@ -51,8 +61,8 @@
             <td>josepborrell@iesebre.com</td>
             <td>12/09/1993</td>
             <td>No</td>
-	    <td><a href="modifyusers/2"><input type="button" name="boton" value="Modificar"/></a>
-	    <a href="deleteusers/2"><input type="button" name="boton" value="Eliminar"/></td></a>
+	    <td><a href="modifyusers/2"><input type="button" name="boton" class="btn btn-sm btn-primary" value="Modificar"/></a>
+	    <a href="deleteusers/2"><input type="button" name="boton" class="btn btn-sm btn-danger" value="Eliminar"/></td></a>
         </tr>
         <tr>
 	    <td>3</td>
@@ -60,13 +70,15 @@
 	    <td>manuelblanch@iesebre.com</td>
 	    <td>25/09/1980</td>
 	    <td>Si</td>
-	    <td><a href="modifyusers/3"><input type="button" name="boton" value="Modificar"/></a>
-	    <a href="deleteusers/3"><input type="button" name="boton" value="Eliminar"/></td></a>
+	    <td><a href="modifyusers/3"><input type="button" name="boton" class="btn btn-sm btn-primary" value="Modificar"/></a>
+	    <a href="deleteusers/3"><input type="button" name="boton" class="btn btn-sm btn-danger" value="Eliminar"/></td></a>
         </tr>
     </tbody>
 </table>
 
-
+<footer>
+        <p align="center">&copy; Albert Canelles Panisello</p>
+	</footer>
 </body>
 
 </html>
--- application/views/pagina.php
+++ application/views/pagina.php
@@ -6,27 +6,31 @@
 		<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
 </head>
 <body>
-<div class="container">
-    <div class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container">
+<div class="navbar navbar-inverse navbar-fixed-top">
+	<div class="container">
         <div class="navbar-header">
           <button data-target=".navbar-collapse" data-toggle="collapse" class="navbar-toggle" type="button">
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a href="#" class="navbar-brand">Project name</a>
+          <a href="#" class="navbar-brand">Menu Principal</a>
         </div>
         <div class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
+            <li class="active"><a href="createusers">Registrat</a></li>
+            <li><a href="createusers">Canvia contrasenya</a></li>
+            <li><a href="modifyusers">Modifica l'usuari</a></li>
           </ul>
-        </div><!--/.nav-collapse -->
+        </div>
       </div>
-    </div>
-
+      </div>
+<div>
+</div>
+</br>
+</br>
+</br>
+</br>
 <ol>
 Usuaris
 <li><a href="http://localhost/codeigniterhelloworld/index.php/users/createusers"> Crea el teu usuari</a></li>
@@ -39,5 +43,9 @@ Configuracio
 <li><a href="http://localhost/codeigniterhelloworld/index.php/config/reset"> Canvia la teva paraula de pas </a></li>
 <li><a href="http://localhost/codeigniterhelloworld/index.php/config/sortir"> Sortir </a></li>
 </ol>
+</div>
+	<footer>
+        <p align="center">&copy; Albert Canelles Panisello</p>
+	</footer>
 </body>
 </html>
