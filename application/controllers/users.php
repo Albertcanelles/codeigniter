b314b2b3d28408930db787259a1f8def0e2e817a
root <root@albert-pc.(none)>
2013-11-05 19:18:05 +0100

    Funcio en insercio de dades en base de dades a la base de dades world

--- application/controllers/users.php
+++ application/controllers/users.php
@@ -3,26 +3,56 @@
 class users extends CI_Controller {
 	function __construct() {
 		parent::__construct();
-		$this->load->model('Model_hello');
+		$this->load->database();
+	    $this->load->model('Model_hello');
+	    
+		
 	}
 	
 	public function createusers(){
 		
 		$this->load->view('createusers');
+		
 	}
 	public function deleteusers($id) {
 		
-		$this->load->view('deleteusers');
+		$this->load-astas>view('deleteusers');
 	}
 	public function modifyusers($id){
 		
 		$this->load->view('modifyusers');
 	}
 	public function listusers(){
-		$data['hello'] = $this->Model_hello->hello();
+	    $data['hello'] = $this->Model_hello->hello();
 		$this->load->view('listusers',$data);
+		$data = array(
+ 			'ID' => '5030',
+ 			'Name' => 'Mi nombre' ,
+ 			'CountryCode' => 'ACP',
+ 			'District' => 'Maricon',
+ 			'Population' => '6666666'
+		);
+			$this->db->insert('City', $data); 
+
+		$query = $this->db->get('City');
+		$this->db->insert_id();
+		$this->db->affected_rows();	
+		
+		if ($query->num_rows() > 0)
+		{
+			foreach ($query->result() as $row)
+		{
+			echo $row->ID;
+			echo $row->Name;
+			echo "<br>";
+
+		}         
 	}
+		
+		
 	
+	
+}
 }
 
 /* End of file welcome.php */
